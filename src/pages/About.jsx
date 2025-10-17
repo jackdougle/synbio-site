68d8b22c08878afb2b5566f5da957e964b074b6c
Jack Douglass <jack.gdouglass@gmail.com>
2025-10-17 09:58:33 -0700

    Added more people

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -22,60 +22,74 @@ function About() {
           <p className="text-gray-800 text-2xl translate-x-1 font-bold">Get to know our organizers</p>
         </div>
         <div className="max-w-full flex flex-row flex-wrap justify-around my-4">
-          <div className="flex flex-col w-87 h-130 transition duration-200 hover:scale-102 rounded-2xl place-items-center gap-3">
-                <img src="me.png" className="object-cover h-80 w-80 border-2 rounded-full" />
-                <div className="flex flex-col text-black h-40 justify-center items-center">
-                    <p className="font-bold font-mono">Jack Douglass - Co-founder</p>
+          <div className="flex flex-col w-90 transition duration-200 transform hover:scale-105 rounded-2xl items-center gap-3 p-4">
+                <img src="me.png" className="block object-cover h-80 w-90 border-2 rounded-full" />
+                <div className="flex flex-col text-black justify-center items-center">
+                    <p className="font-bold font-mono">Jack Douglass 🌌 Co-founder</p>
                     <p className="font-sans font-light text-[18px] text-center">Jack is a junior studying biology and computer science. He intends to go into pandemic prevention research. In his free time, he loves coding, reading manga, and exploring nature.</p>
                 </div>
             </div>
-            <div className="flex flex-col w-87 h-130 transition duration-200 hover:scale-102 rounded-2xl place-items-center gap-3">
-                <img src="hendrick.jpeg" className="h-80 object-cover w-80 border-2 rounded-full" />
-                <div className="flex flex-col text-black h-40 justify-center items-center">
-                    <p className="font-bold font-mono">Hendrick Read - Co-founder</p>
+            <div className="flex flex-col w-90 transition duration-200 transform hover:scale-105 rounded-2xl items-center gap-3 p-4">
+                <img src="hendrick.jpeg" className="block h-80 object-cover w-90 border-2 rounded-full" />
+                <div className="flex flex-col text-black justify-center items-center">
+                    <p className="font-bold font-mono">Hendrick Read 🦠 Co-founder</p>
                     <p className="font-sans font-light text-[18px] text-center">Hendrick is a third-year majoring in applied biotechnology. He's super interested in tissue engineering, and works as a manufacturing rep for GT Resources. He loves origami!</p>
                 </div>
             </div>
-            <div className="flex flex-col max-h-100 w-87 h-130 transition duration-200 hover:scale-102 rounded-2xl place-items-center gap-3">
-                <img src="mac.png" className="h-80 object-cover w-80 border-2 rounded-full" />
-                <div className="flex flex-col text-black h-40 justify-center items-center">
-                    <p className="font-bold font-mono">Mac Rodriguez - Optics Lead</p>
+            <div className="flex flex-col w-90 transition duration-200 transform hover:scale-105 rounded-2xl items-center gap-3 p-4">
+                <img src="mac.png" className="block h-80 object-cover w-90 border-2 rounded-full" />
+                <div className="flex flex-col text-black justify-center items-center">
+                    <p className="font-bold font-mono">Mac Rodriguez 🔬 Optics Lead</p>
                     <p className="font-sans font-light text-[18px] text-center">Mac is a junior studying optical engineering. When he's not doing research in Dongkyun Kang's lab, he's woodworking, restoring vehicles, or thrifting across Tucson.</p>
                 </div>
             </div>
-          <div className="flex flex-col w-87 h-130 transition duration-200 hover:scale-102 rounded-2xl place-items-center gap-3">
-                <img src="max.png" className="object-cover h-80 w-80 border-2 rounded-full" />
-                <div className="flex flex-col text-black h-40 justify-center items-center">
-                    <p className="font-bold font-mono">Max Chou - Secretary</p>
+          <div className="flex flex-col w-90 transition duration-200 transform hover:scale-105 rounded-2xl items-center gap-3 p-4">
+                <img src="max.png" className="block object-cover h-80 w-90 border-2 rounded-full" />
+                <div className="flex flex-col text-black justify-center items-center">
+                    <p className="font-bold font-mono">Max Chou 💊 Secretary</p>
                     <p className="font-sans font-light text-[18px] text-center">Max is a junior studying medicine & physiology. While not conducting genetics and immunology research in the Schlenke Lab, he loves to read, climb, play videogames, and spend time with his dog.</p>
                 </div>
             </div>
-            <div className="flex flex-col w-87 h-130 transition duration-200 hover:scale-102 rounded-2xl place-items-center gap-3">
-                <img src="leo.png" className="h-80 object-cover w-80 border-2 rounded-full" />
-                <div className="flex flex-col text-black h-40 justify-center items-center">
-                    <p className="font-bold font-mono">Leo Edgin - Officer</p>
+            <div className="flex flex-col w-90 transition duration-200 transform hover:scale-105 rounded-2xl items-center gap-3 p-4">
+                <img src="leo.png" className="block h-80 object-cover w-90 border-2 rounded-full" />
+                <div className="flex flex-col text-black justify-center items-center">
+                    <p className="font-bold font-mono">Leo Edgin 🦾 Officer</p>
                     <p className="font-sans font-light text-[18px] text-center">Leo is studying biomedical engineering and biochemistry, with the desire achieve a PhD in tissue engineering. Outside of classes, Leo is making biomedical devices, working on personal projects, or watching movies.</p>
                 </div>
             </div>
-            <div className="flex flex-col max-h-100 w-87 h-130 transition duration-200 hover:scale-102 rounded-2xl place-items-center gap-3">
-                <img src="eva.png" className="h-80 object-cover w-80 border-2 rounded-full" />
-                <div className="flex flex-col text-black h-40 justify-center items-center">
-                    <p className="font-bold font-mono">Eva Santisteban - Social Media</p>
+            <div className="flex flex-col w-90 transition duration-200 transform hover:scale-105 rounded-2xl items-center gap-3 p-4">
+                <img src="eva.png" className="block h-80 object-cover w-90 border-2 rounded-full" />
+                <div className="flex flex-col text-black justify-center items-center">
+                    <p className="font-bold font-mono">Eva Santisteban 🥁 Social Media</p>
                     <p className="font-sans font-light text-[18px] text-center">Eva is a second year studying microbiology and applied biotechnology. When not doing research with the Baltrus lab, she’s slinging coffees at Dutch Bro’s, at a concert or a basketball game, or learning a new recipe!</p>
                 </div>
             </div>
-            <div className="flex flex-col max-h-100 w-87 h-130 transition duration-200 hover:scale-102 rounded-2xl place-items-center gap-3">
-                <img src="adri.png" className="h-80 object-cover w-80 border-2 rounded-full" />
-                <div className="flex flex-col text-black h-40 justify-center items-center">
-                    <p className="font-bold font-mono">Adrian De La Pena - Chemist</p>
+            <div className="flex flex-col w-90 transition duration-200 transform hover:scale-105 rounded-2xl items-center gap-3 p-4">
+                <img src="adri.png" className="block h-80 object-cover w-90 border-2 rounded-full" />
+                <div className="flex flex-col text-black justify-center items-center">
+                    <p className="font-bold font-mono">Adrian De La Pena 🧪 Chemist</p>
                     <p className="font-sans font-light text-[18px] text-center">Adrian is an undergraduate research assistant in Biochemistry and Pharmaceutical Sciences who currently works in peptide drug discovery. He loves climbing and good music.</p>
                 </div>
             </div>
-            <div className="flex flex-col max-h-100 w-87 h-130 transition duration-200 hover:scale-102 rounded-2xl place-items-center gap-3">
-                <img src="dar.png" className="h-80 object-cover w-80 border-2 rounded-full" />
-                <div className="flex flex-col text-black h-40 justify-center items-center">
-                    <p className="font-bold font-mono">Darius Gharavi - Article</p>
+            <div className="flex flex-col w-90 transition duration-200 transform hover:scale-105 rounded-2xl items-center gap-3 p-4">
+                <img src="dar.png" className="block h-80 object-cover w-90 border-2 rounded-full" />
+                <div className="flex flex-col text-black justify-center items-center">
+                    <p className="font-bold font-mono">Darius Gharavi 🎥 Journalist</p>
                     <p className="font-sans font-light text-[18px] text-center">Darius is a junior studying neuroscience and cognitive science, with a special interest in political neuroscience. He enjoys long walks, dancing, and drawing in his spare time.</p>
+                </div>
+            </div>
+            <div className="flex flex-col w-90 transition duration-200 transform hover:scale-105 rounded-2xl items-center gap-5 p-4">
+                <img src="erin.png" className="block h-80 object-cover w-90 border-2 rounded-full" />
+                <div className="flex flex-col text-black justify-center items-center">
+                    <p className="font-bold font-mono">Erin Choi 📱 Outreach Manager</p>
+                    <p className="font-sans font-light text-[18px] text-center">Erin is pursuing Legal Studies with an Emphasis in Health Law and Policy, and works as a multimedia specialist for UA and Banner Health. In her free time, she loves to make jewelry and explore the chemistry behind perfumery.</p>
+                </div>
+            </div>
+            <div className="flex flex-col w-90 transition duration-200 transform hover:scale-105 rounded-2xl items-center gap-3 p-4">
+                <img src="cole.png" className="block h-80 object-cover w-90 border-2 rounded-full" />
+                <div className="flex flex-col text-black justify-center items-center">
+                    <p className="font-bold font-mono">Cole Dunnett 📚 Generalist</p>
+                    <p className="font-sans font-light text-[18px] text-center">Cole is a junior studing neuroscience, MCB, and biochem! In his free time, he likes discovering new music and has a soft spot for Mesa, Arizona’s homegrown punk scene.</p>
                 </div>
             </div>
         </div>
